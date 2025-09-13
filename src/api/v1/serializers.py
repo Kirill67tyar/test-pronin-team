7dcbf0a3c5c28dd435dbb3f93c571b6bfa04b421
Kirill67tyar <kirillbogomolov.ric@gmail.com>
2025-09-13 18:56:21 +0300

    Creates serializers and modelviewsets
    Constructs optimal queryset for avoid n+1

--- src/api/v1/serializers.py
+++ src/api/v1/serializers.py
@@ -2,10 +2,32 @@ from rest_framework import serializers
 
 from django.contrib.auth import get_user_model
 from fees.models import Collect, Payment
+from decimal import Decimal
 
 User = get_user_model()
 
+class UserModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+        )
 class PaymentModelSerializer(serializers.ModelSerializer):
+    """
+    action list:
+        id
+        name
+        owner (nested или id/username)
+        collection (id или brief info, как name коллекции)
+        amount
+        created_at
+        
+    action detail:
+        Все из списка выше.
+        updated_at
+        Полный collection (nested serializer с деталями коллекции, если нужно).
+    """
     class Meta:
         model = Payment
         fields = (
@@ -18,8 +40,46 @@ class PaymentModelSerializer(serializers.ModelSerializer):
             'updated_at',
         )
 
-class CollectModelSerializer(serializers.ModelSerializer):
 
+class CollectListModelSerializer(serializers.ModelSerializer):
+    """
+    action list:
+        id (автоматически, как PK)
+        name
+        author (сериализованный как nested или просто username/id, в зависимости от нужд)
+        cause (с readable label из choices, если используешь get_cause_display)
+        planned_amount
+        image (URL, если используешь ImageField)
+        closing_to
+        TODO current_amount_in_percent для этого нужно вычислять current_amount и вычислять по формуле
+    """
+    cause_display = serializers.CharField(source='get_cause_display', read_only=True)
+    author = UserModelSerializer(
+        many=False,
+    )
+    class Meta:
+        model = Collect
+        fields = (
+            "id",
+            "name",
+            "author",
+            "cause",
+            "cause_display",
+            "planned_amount",
+            "image",
+            "closing_to",
+        )
+        
+    """
+    action detail:
+        Все из списка выше.
+        description (полный текст).
+        current_amount (вычисляемый: sum(Payment.amount for payment in self.payments.all()) via SerializerMethodField).
+        count_supporters (вычисляемый: self.payments.values('owner').distinct().count()).
+        Опционально: список связанных payments (nested serializer, но с pagination, если их много) или просто ссылки на них.
+        Если нужно, добавь list_supported как method field, возвращающий список owners из payments.
+    """
+class CollectDetailModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collect
         fields = (
@@ -29,7 +89,55 @@ class CollectModelSerializer(serializers.ModelSerializer):
             'cause',
             'description',
             'planned_amount',
+            'current_amount',
+            'supporters_full_names',
             'image',
             'closing_to',
+            'count_supporters',
         )
+    author = UserModelSerializer(
+        many=False,
+    )
+    count_supporters = serializers.SerializerMethodField(
+        read_only=True,
+    )
+    current_amount = serializers.SerializerMethodField(
+        read_only=True,
+    )
+    supporters_full_names = serializers.SerializerMethodField(
+        read_only=True,
+    )
+    
+    def get_count_supporters(self, obj):
+        """Количество человек сделавших пожертвования."""
+        return getattr(obj, "count_supporters", 0)
+    
+    def get_current_amount(self, obj):
+        """Количество человек сделавших пожертвования."""
+        return getattr(obj, "current_amount", Decimal("0.00"))
+    
+    def get_supporters_full_names(self, obj):
+        """Количество человек сделавших пожертвования."""
+        return getattr(obj, "supporters_full_names", [])
 
+
+class CollectWriteModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collect
+        fields = (
+            "name",
+            "description",
+            "cause",
+            "planned_amount",
+            "closing_to",
+            "image",
+        )
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        return super().create(validated_data)
+
+
+    def to_representation(self, value):
+        serializer = CollectDetailModelSerializer(value)
+        serializer.context['request'] = self.context['request']
+        return serializer.data
--- src/api/v1/views.py
+++ src/api/v1/views.py
@@ -1,6 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 from fees.models import Collect, Payment
-from api.v1.serializers import CollectModelSerializer, PaymentModelSerializer
+from api.v1.serializers import (CollectDetailModelSerializer, CollectListModelSerializer, PaymentModelSerializer, CollectWriteModelSerializer)
+from django.db.models import Count, OuterRef, Subquery, Value, Sum
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.db.models.functions import Coalesce, Concat
+from django.contrib.postgres.fields import ArrayField
+from django.db.models.fields import CharField
+from decimal import Decimal
+
 
 class PaymentModelViewSet(ModelViewSet):
     serializer_class = PaymentModelSerializer
@@ -8,16 +15,56 @@ class PaymentModelViewSet(ModelViewSet):
 
 
 class CollectModelViewSet(ModelViewSet):
-    serializer_class = CollectModelSerializer
-    queryset = Collect.objects.prefetch_related("payments", "payments__owner")
-    # filter_backends = (SearchFilter,)
-    # search_fields = ("username",)
+    serializer_class = CollectDetailModelSerializer
+    queryset = Collect.objects.select_related("author")
     # permission_classes = (IsAdminOrOwner,)
-    # lookup_field = "username"
-    # lookup_url_kwarg = "username"
     http_method_names = [
         "get",
         "post",
         "patch",
         "delete",
     ]
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return CollectListModelSerializer
+        if self.action == 'retrieve':
+            return CollectDetailModelSerializer
+        return CollectWriteModelSerializer
+        
+    def get_queryset(self):
+        current_amount_subquery = Subquery(
+            Payment.objects.filter(collection=OuterRef('pk'))
+            .values('collection')
+            .annotate(total=Sum('amount'))
+            .values('total')
+        )
+        queryset = super().get_queryset().annotate(
+            current_amount=Coalesce(current_amount_subquery, Value(Decimal('0.00')))
+            )
+        if self.action == "retrieve":
+            count_subquery = Subquery(
+                Payment.objects.filter(collection=OuterRef('pk'))
+                .values('collection')
+                .annotate(count=Count('owner', distinct=True))
+                .values('count')
+            )
+
+            names_subquery = Subquery(
+                Payment.objects.filter(collection=OuterRef('pk'))
+                .values('collection')
+                .annotate(
+                    full_names=ArrayAgg(
+                        Concat('owner__first_name', Value(' '), 'owner__last_name', output_field=CharField()),
+                        distinct=True
+                    )
+                )
+                .values('full_names')
+            )
+            queryset = queryset.annotate(
+                count_supporters=Coalesce(count_subquery, Value(0)),
+                supporters_full_names=Coalesce(
+                    names_subquery,
+                    Value([], output_field=ArrayField(CharField()))
+                ),
+            )
+        return queryset
