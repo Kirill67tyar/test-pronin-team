5cc4030a6e66f34ac5aa063ed3b3fd5718882f2e
Kirill67tyar <kirillbogomolov.ric@gmail.com>
2025-09-17 00:31:54 +0300

    Removes comments

--- src/core/settings.py
+++ src/core/settings.py
@@ -83,9 +83,9 @@ REST_FRAMEWORK = {
     ),
 }
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),  # Короткий access
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),  # Длинный refresh
-    "ROTATE_REFRESH_TOKENS": False,  # Не ротировать (опционально, если не нужно)
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": True,
     "AUTH_HEADER_TYPES": ("Bearer",),
